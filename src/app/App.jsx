2990c3a57f9b2e92094cd1ed505c962f37100493
NOAH <noah@reinvention.io>
2018-02-14 23:10:00 +0700

    chore: Config for absolute path import

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Signup from '../pages/Signup';
-import './App.css';
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import Signup from 'pages/Signup';
+import 'app/App.css';
 
 const App = () => (
   <div id="app">
