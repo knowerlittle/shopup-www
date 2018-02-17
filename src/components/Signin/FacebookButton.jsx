826adf396cdc6015d4cfec35cb8969b0e20ebc22
NOAH <noah@reinvention.io>
2018-02-17 21:22:00 +0700

    chore: Remove ignored files in .gitignore in commits

--- src/components/Signin/FacebookButton.jsx
+++ src/components/Signin/FacebookButton.jsx
@@ -3,11 +3,14 @@ import SocialButton from 'components/Signin/SocialButton';
 import handleFacebookLogin from 'api/auth/facebook';
 import handleSocialLoginFailure from 'api/auth/handleSocialLoginFailure';
 
+const FACEBOOK = 'facebook';
+const APP_ID = process.env.FACEBOOK_APP_ID;
+
 const FacebookButton = () => (
   <div>
     <SocialButton
-      provider="facebook"
-      appId="149261575830134"
+      provider={FACEBOOK}
+      appId={APP_ID}
       onLoginSuccess={handleFacebookLogin}
       onLoginFailure={handleSocialLoginFailure}
     >
