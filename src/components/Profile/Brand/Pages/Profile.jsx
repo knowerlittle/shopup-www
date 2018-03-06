6ad6f5d6a172b2867827be28d3367babaa8d0a77
NOAH <noah@reinvention.io>
2018-03-06 23:00:22 +0700

    feat: Create CSS grid profile Info

--- src/components/Profile/Brand/Pages/Profile.jsx
+++ src/components/Profile/Brand/Pages/Profile.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from 'components/Profile/index.css';
+import BackgroundDemo from 'assets/img/demo/brandbg.png';
+
+const divStyle = {
+  backgroundImage: `url(${BackgroundDemo}`,
+  backgroundSize: 'cover',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+};
 
 const Profile = () => (
-  <div>
+  <div className={styles.pageWrapper}>
+    <section className={styles.profileInfo} style={divStyle}>
+      <div className={styles.profileItemTop}>
+        Hi 
+      </div>
+      <div className={styles.profileItem}>Hi</div>
+      <div className={styles.profileItem}>Hi</div>
+      <div className={styles.profileItem}>Hi</div>
+      <div className={styles.profileItemBottom}>Hi</div>
+    </section>
     Profilex
   </div>
 );
