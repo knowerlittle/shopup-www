9b24f42586b98c9919733e71b3e630701b9ff296
NOAH <noah@reinvention.io>
2018-03-06 23:45:15 +0700

    WIP: Profile info

--- src/components/Profile/Brand/Pages/Profile.jsx
+++ src/components/Profile/Brand/Pages/Profile.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import styles from 'components/Profile/index.css';
+import SVG from 'react-inlinesvg';
+import Email from 'assets/icons/ic_email.svg';
 import BackgroundDemo from 'assets/img/demo/brandbg.png';
+import HatLady from 'assets/img/demo/hatdemo.png';
 
 const divStyle = {
   backgroundImage: `url(${BackgroundDemo}`,
@@ -8,13 +13,23 @@ const divStyle = {
   backgroundColor: 'rgba(0, 0, 0, 0.5)',
 };
 
-const Profile = () => (
+const brandDescription = `Co-founder, chairman, and chief executive officer (CEO) 
+    of Apple Inc CEO and majority shareholder of Pixar Animation Studios; 
+    a member of The Walt Disney Company's`;
+
+const Profile = ({ brand }) => (
   <div className={styles.pageWrapper}>
     <section className={styles.profileInfo} style={divStyle}>
       <div className={styles.profileItemTop}>
-        Hi 
+        <span className={styles.brandName}>{brand.name}</span>
+        <div className={styles.purpleCircle}>
+          <SVG src={Email} />
+        </div>
       </div>
-      <div className={styles.profileItem}>Hi</div>
+      <div className={styles.profileItemWithImage}>
+        <img src={HatLady} alt="Brand" width="120" />
+        <p>{brandDescription}</p>
+      </div>
       <div className={styles.profileItem}>Hi</div>
       <div className={styles.profileItem}>Hi</div>
       <div className={styles.profileItemBottom}>Hi</div>
@@ -23,4 +38,15 @@ const Profile = () => (
   </div>
 );
 
-export default Profile;
+Profile.propTypes = {
+  brand: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+const mapStateToProps = (state) => {
+  const { brand } = state.auth;
+  return {
+    brand,
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
