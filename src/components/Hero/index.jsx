c703905b7f1c2faec9a5eb8118bab41a8b5d5084
NOAH <noah@reinvention.io>
2018-03-07 02:17:46 +0700

    feat: Added homepage image and text

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 import Button from 'components/Global/Button';
 import styles from 'components/Hero/index.css';
 import { Link } from 'react-router-dom';
+import NeonLike from 'assets/img/home/neonlike.png';
+
+const introText = 'THE INTERNET SAYS IT\'S';
 
 const Hero = () => (
   <div>
     <section className={styles.heroContainer}>
-      <h1>Home page title</h1>
-      <Link to="/signup">
-        <Button
-          color="purple"
-          text="GET STARTED"
-          height="50"
-          width="300"
-        />
-      </Link>
+      <div className={styles.leftSection}>
+        <p className={styles.introText}>{introText}</p>
+        <p className={styles.mainText}>The best way</p>
+        <p className={styles.mainText}>to find space</p>
+        <p className={styles.mainTextLast}>for your passion.</p>
+        <div className={styles.buttonWrapper}>
+          <Link to="/signup">
+            <Button
+              color="purple"
+              text="GET STARTED"
+              height="50"
+              width="300"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.rightSection}>
+        <img src={NeonLike} alt="like" />
+      </div>
     </section>
     <section className={styles.featureContainer}>
       <h2>Feature Container</h2>
