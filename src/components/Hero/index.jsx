9c6ad80f6d21625f8bd990d8b8c2039bc450b165
NOAH <noah@reinvention.io>
2018-03-08 02:56:08 +0700

    feat: Add iframe and thinglink

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Button from 'components/Global/Button';
 import styles from 'components/Hero/index.css';
 import { Link } from 'react-router-dom';
-import NeonLike from 'assets/img/home/neonlike.png';
+// import NeonLike from 'assets/img/home/neonlike.png';
+import Iframe from 'react-iframe';
 
 const introText = 'THE INTERNET SAYS IT\'S';
 
@@ -26,7 +27,17 @@ const Hero = () => (
         </div>
       </div>
       <div className={styles.rightSection}>
-        <img src={NeonLike} alt="like" />
+        {/* <img src={NeonLike} alt="like" /> */}
+        <div className={styles.videoWrapper}>
+          <Iframe
+            url="https://www.thinglink.com/mediacard/951052997931040769?autoplay=1&autorotate=1&displaytitle=1&rel=1"
+            id="myId"
+            className="myClassname"
+            display="initial"
+            position="relative"
+            allowFullScreen
+          />
+        </div>
       </div>
     </section>
     <section className={styles.featureContainer}>
